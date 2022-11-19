8bae6c97f5fb79e943489137ab2965cbcc4dad4e
long_nh <long_nh@flinters.vn>
2022-11-19 14:44:21 +0700

    update

--- components/DE/allApplets.js
+++ components/DE/allApplets.js
@@ -4,8 +4,8 @@ import appLauncher from '../apps/appLauncher.js'
 export const ALL_APPLETS = [
     {
         id: 'all-apps',
-        position: [null, null, '90px', 'calc(50% - 300px)'],
-        content: '<div class="app-drawer" style="width:500px;max-height:600px"></div>',
+        position: [null, null, '90px', 'calc(50% - 290px)'],
+        content: '<div class="app-drawer" style=""></div>',
         func: appLauncher
     },
     {
